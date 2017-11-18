64c622c853ac8cf798515c76b117ee10eb2ed6fb
underludrew <31699868+underludrew@users.noreply.github.com>
2017-11-18 17:33:34 +0800

    Add files via upload

---
+++ Account.java
@@ -0,0 +1,9 @@
+public class Account{
+	private String accoutNo;
+	private double balance;
+	public Account(){}
+	public Account(String accountNo,double balance){
+		this.accountNo=accountNo;
+		this.balance=balance;
+	}
+}
